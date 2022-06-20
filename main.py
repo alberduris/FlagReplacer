24813a60750a8cc47426e558b8b88ed3cda395ac
Alber <4lb3rduris@gmail.com>
2022-06-20 13:13:23 +0200

    fixes textos y control de errores de archivos

--- main.py
+++ main.py
@@ -11,28 +11,28 @@ def convert_df(df):
     return df.to_csv(index=False, header=None).encode('utf-8')
 
 st.title('Flag Replacer')
-st.header('Powered by Joseo20')
+st.header('Joseo20')
 
 with st.expander('Instrucciones de uso'):
     st.markdown("""
-Reemplaza las BANDERAs encontradas en el archivo de CONTENIDO con las palabras claves del archivo KEYWORDS con correspondencia de líneas 1:1
+Reemplaza las BANDERAs encontradas en el archivo de CONTENIDOS con las palabras claves del archivo KEYWORDS con correspondencia de líneas 1:1
 
 Se espera:
 
-- Un archivo CONTENIDO con un texto por línea que puede tener una BANDERA que será sustituida
+- Un archivo CONTENIDOS con un texto por línea que puede tener una BANDERA que será sustituida
 - Un archivo KEYWORDS con una palabra clave por línea que será sustituida por la BANDERA del archivo CONTENIDO
 
 Genera:
 
-- Un archivo REEMPLAZADO con los textos de CONTENIDO pero las BANDERAS sustituidas por las KEYWORDS, línea a línea
+- Un archivo REEMPLAZADO con los textos de CONTENIDOS pero las BANDERAS sustituidas por las KEYWORDS, línea a línea
     """)
 
 
-st.subheader('File input')
+st.subheader('Entrada de archivos')
 
-st.caption('Make sure that the contents and keywords file have the same number of lines. If one is longer, the extra files from the longer will be ignored.')
+st.caption('Asegúrate de que el archivo de CONTENIDO y KEYWORDS tiene el mismo número de líneas. Si uno de los dos archivos contiene mayor número de filas, las filas extra del archivo más largo serán descartadas.')
 
-contents_file = st.file_uploader(label='Contents', type=['txt','csv','xlsx'], key='contents_file_uploader', help='Upload the CONTENTS file, with one post per line')
+contents_file = st.file_uploader(label='Contenidos', type=['txt','csv','xlsx'], key='contents_file_uploader', help='Sube el archivo de CONTENIDOS, con un post por línea')
 # Previsualización file
 if contents_file is not None:
     content = StringIO(contents_file.getvalue().decode("utf-8")).read().splitlines()
@@ -40,7 +40,7 @@ if contents_file is not None:
     with st.expander('Inspect your contents file:'):
         st.table(dfContent.head())
 
-keywords_file = st.file_uploader(label='Keywords', type=['txt','csv','xlsx'], key='kws_file_uploader', help='Upload the KEYWORDS file, with one keyword per line')
+keywords_file = st.file_uploader(label='Keywords', type=['txt','csv','xlsx'], key='kws_file_uploader', help='Sube el archivo de KEYWORDS, con una keyword por línea')
 # Previsualización file
 if keywords_file is not None:
     keywords = StringIO(keywords_file.getvalue().decode("utf-8")).read().splitlines()
@@ -48,29 +48,35 @@ if keywords_file is not None:
     with st.expander('Inspect your keywords file:'):
         st.table(dfKeywords.head())
 
-st.subheader('Keywords flag')
-FLAG = st.text_input('Set your desired flag', value='%&AAA%&', key='flag_input', help='Sets the flag, i.e., string to be replaced on the content file.')
+st.subheader('Bandera para las keywords')
+FLAG = st.text_input('Escribe la bandera a utilizar', value='%&AAA%&', key='flag_input', help='Asigna la bandera, e.g., el string de caracteres a ser reemplazado en el archivo de contenidos.')
 
 st.markdown("---")
 
-st.subheader('Replacer')
-replace_flags_btn = st.button('Replace flags')
+st.subheader('Reemplazador')
+replace_flags_btn = st.button('Reemplazar banderas por keywords')
 if replace_flags_btn:
 
-    dfContent['REPLACED'] = dfContent.apply(lambda x: replace_flag(x, FLAG, keywords), axis=1)
-    # Previsualización OUTPUT
-    if dfContent['REPLACED'] is not None:
-        st.image('https://emoji-uc.akamaized.net/orig/f7/f1297a3ffec34c824497db1e3ca51f.png', width=30)
-        with st.expander('Inspect your output file:'):
-            st.table(dfContent["REPLACED"].head())
+    if keywords_file is not None and contents_file is not None:
+        dfContent['REPLACED'] = dfContent.apply(lambda x: replace_flag(x, FLAG, keywords), axis=1)
+        # Previsualización OUTPUT
+        if dfContent['REPLACED'] is not None:
+            with st.expander('Inspect your output file:'):
+                st.text('El orangotán cósmeco te bendice con un archivo con banderas reemplazadas'); st.image('https://emoji-uc.akamaized.net/orig/f7/f1297a3ffec34c824497db1e3ca51f.png', width=30)
+                st.table(dfContent["REPLACED"].head())
 
-    # Save file
-    csv = convert_df(dfContent['REPLACED'])
+        # Save file
+        csv = convert_df(dfContent['REPLACED'])
 
-    download = st.download_button(label='👉 Download replaced contents file', data=csv, mime='text/csv')
-
-    if download is not None:
-        st.balloons()
+        download = st.download_button(label='👉 Descarga el archivo de CONTENIDOS con los reemplazos efectuados', data=csv, mime='text/csv')
+    else:
+        if contents_file is None and keywords_file is not None:
+            st.warning('⚠️ Falta el archivo de CONTENIDOS. Por favor, sube un archivo de CONTENIDOS válido.')
+        elif keywords_file is None and contents_file is not None:
+            st.warning('⚠️ Falta el archivo de KEYWORDS. Por favor, sube un archivo de KEYWORDS válido.')
+        else:
+            st.warning('⚠️ Hay un problema con los archivos. Por favor, sube un archivo de CONTENIDOS y otro de KEYWORDS.')
+            
 
 
 
